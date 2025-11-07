25ec28d48149e0464ec26e1642a552153f2d225e
Diego Massarni <webdiegomassarini@gmail.com>
2025-11-07 13:11:38 +0100

    wip: animation

--- components/WhoIAm.tsx
+++ components/WhoIAm.tsx
@@ -1,33 +1,46 @@
+"use client";
+import * as React from "react";
 import { Button } from "./ui/button";
+import { motion, useInView } from "framer-motion";
+
 export default function WhoIAm() {
+  const ref = React.useRef(null);
+
+  const isInView = useInView(ref);
+
   return (
-    <div
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 20 }}
+      animate={isInView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.5, delay: 0.5 }}
       id="whoiam"
-      className="bg-white max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 py-20 px-4"
     >
-      <div>
+      <div className="bg-white max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 py-20 px-4">
         <div>
-          <p className="font-black uppercase mb-1">
-            <span>|</span>Who I am
+          <div>
+            <p className="font-black uppercase mb-1">
+              <span>|</span>Who I am
+            </p>
+          </div>
+          <div className="uppercase font-black text-5xl  leading-10">
+            <h2>Samuele </h2>
+            <h2>Spriano</h2>
+          </div>
+        </div>
+        <div className="mt-5 md:mt-0 ">
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
+            labore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Fuga iusto porro laboriosam autem beatae deserunt maiores libero
+            praesentium dolore cumque. Quae, iste dolorum facilis distinctio
+            velit eos autem voluptate debitis.
           </p>
-        </div>
-        <div className="uppercase font-black text-5xl  leading-10">
-          <h2>Samuele </h2>
-          <h2>Spriano</h2>
+          <Button className="mt-4">
+            <p className="uppercase font-bold text-xs">Get started</p>
+          </Button>
         </div>
       </div>
-      <div className="mt-5 md:mt-0 ">
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, labore.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga iusto
-          porro laboriosam autem beatae deserunt maiores libero praesentium
-          dolore cumque. Quae, iste dolorum facilis distinctio velit eos autem
-          voluptate debitis.
-        </p>
-        <Button className="mt-4">
-          <p className="uppercase font-bold text-xs">Get started</p>
-        </Button>
-      </div>
-    </div>
+    </motion.div>
   );
 }
