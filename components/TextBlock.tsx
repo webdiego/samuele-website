31d25af3211f34a88bc624dcdd3f85a350a356a8
Diego Massarni <webdiegomassarini@gmail.com>
2025-11-09 10:09:34 +0100

    fix: iframe style

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -46,13 +46,14 @@ const VideoComponent = ({ value }: { value: VideoValue }) => {
   const embedUrl = getEmbedUrl(value.url);
 
   return (
-    <div className="my-6 aspect-video">
+    <div className="my-6 aspect-video w-full">
       {embedUrl ? (
         <iframe
           src={embedUrl}
           title="Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
+          className="w-full h-full rounded-lg"
         />
       ) : (
         <p>Invalid video URL</p>
